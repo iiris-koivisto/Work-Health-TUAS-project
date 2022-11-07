be851cdde7dece33fad3a3e98fec5766eb5a5273
mamyllar <79257764+mamyllar@users.noreply.github.com>
2022-11-07 14:17:54 +0200

    Just to create a merge conflict

--- WorkHealthTUAS/WorkHealth/admin.py
+++ WorkHealthTUAS/WorkHealth/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(Survey)
+admin.site.register(Option)
+admin.site.register(Result)
+admin.site.register(Conflict)
+admin.site.register(Admin)
+admin.site.register(Question)
+admin.site.register(Staff)
