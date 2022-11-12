9a2b6f14f0edacfc89e0292a028044b2d9cafe8c
Iiris Koivisto <koivisto.irkku2gmail.com>
2022-11-12 18:35:54 +0200

    WHT-73 Rendered survey to index.html

--- WorkHealthTUAS/WorkHealth/views.py
+++ WorkHealthTUAS/WorkHealth/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from .models import Survey
+
 # Create your views here.
 
 def index(request):
     #The home page
     return render(request, 'WorkHealth/index.html')
+
+def surveys(request):
+    #Show that week's survey
+    surveys = Survey.objects
+    context = {'surveys':surveys}
+    return render(request, 'WorkHealth/index.html', context)
